1344c1891518301a0e7cbd4882c4fcd230366fca
Pol Pascual <polclarence20@gmail.com>
2026-08-25 01:07:27 +0800

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,43 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  selectedLearner: string = '';
+  selectedReportType: string = '';
+  score: number | null = null;
 
-  constructor() {}
+  learners: string[] = [
+    'John Doe',
+    'Jake Peralta',
+    'Charles Boyle',
+    'Amy Santiago',
+    'Rosa Diaz'
+  ];
 
+  reportTypes: string[] = [
+    'Seatwork',
+    'Take Home Assignment',
+    'Daily Progress Report',
+    'Annual Performance Review'
+  ];
+
+  isSubmitted: boolean = false;
+  submittedData: any = null;
+
+  onSubmit() {
+    this.submittedData = {
+      learner: this.selectedLearner,
+      reportType: this.selectedReportType,
+      score: this.score
+    };
+    this.isSubmitted = true;
+  }
+
+  resetForm() {
+    this.selectedLearner = '';
+    this.selectedReportType = '';
+    this.score = null;
+    this.isSubmitted = false;
+    this.submittedData = null;
+  }
 }
+
